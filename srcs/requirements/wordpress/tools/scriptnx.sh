0a887ae1c77799ca30214fe8a465dbdc1817955b
Opal <opaltales_@windowslive.com>
2025-03-21 21:41:29 +0700

    test2

--- srcs/requirements/wordpress/tools/scriptnx.sh
+++ srcs/requirements/wordpress/tools/scriptnx.sh
@@ -11,7 +11,7 @@ sed -i -r "s/database_name_here/$MRDB_NAME/1"   wp-config.php
 sed -i -r "s/username_here/$MRDB_USER/1"  wp-config.php
 sed -i -r "s/password_here/$MRDB_PASS/1"    wp-config.php
 sed -i -r "s/localhost/mariadb/1"    wp-config.php
-wp core install --url=$DOMAIN_NAME/ --title=$WP_NAME --admin_user=$WP_AD_USR --admin_password=$WP_AD_PASS --admin_email=$WP_AD_EMAIL --skip-email --allow-root
+wp core install --url=$DOMAIN_NAME/ --title=$WP_NAME --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 
 exec "$@"
